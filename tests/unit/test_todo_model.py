16c770035faa68d60f31ba8572c27e481c440a9e
manuelrico659 <manuelrico659@gmail.com>
2024-12-11 13:16:43 -0600

    unit test modification

--- tests/unit/test_todo_model.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from app import cipher_suite, Todo  # Importación directa de `cipher_suite` y `Todo` desde `app.py`
-
-def test_todo_encryption():
-    todo_name = 'Test Todo'
-    encrypted_name = cipher_suite.encrypt(todo_name.encode()).decode()
-    todo = Todo(name=encrypted_name)
-    decrypted_name = todo.decrypt_name()
-    assert decrypted_name == todo_name
